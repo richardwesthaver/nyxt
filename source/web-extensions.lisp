fbc812ebb906f03d94f1259b02a35dc7d8d408ed
Artyom Bologov <aartaka@protonmail.com>
2023-07-20 15:37:11 +0400

    web-extensions: Indent fix.

--- source/web-extensions.lisp
+++ source/web-extensions.lisp
@@ -3,7 +3,7 @@
 
 (nyxt:define-package :nyxt/web-extensions
     (:shadow #:background-buffer)
-  (:documentation "WebExtensions API conformance code."))
+    (:documentation "WebExtensions API conformance code."))
 (in-package :nyxt/web-extensions)
 
 ;; TODO: We need to allow modifying :nyxt form here, for instance because we
